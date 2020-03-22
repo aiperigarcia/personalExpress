4207bccf3e81d1164710eaa150aeade1be80ce3d
aiperieldar <aiperieldar.dev@gmail.com>
2020-03-22 14:13:40 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# personalExpress
+# Frozen Quotes
+
+## Goal:
+
+Create an app where users can post quotes from cartoon Frozen, see what Olaf would response to quotes and delete quote.
+
+![olaf](public/read.png)
+
+## How It's Made:
+
+**Tech Used**: HTML5, CSS3, APIs, Javascript, Node.js, MongoDB, Express framework
+
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:3000`
+
+## Credit
+
+Modified from Scotch.io's auth tutorial
---
+++ public/main.js
@@ -0,0 +1,34 @@
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Olaf',
+      'quote': 'I like warm hugs.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Olaf'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
